1f9f5b7b91eafa33096f0478ffa094480a2d0f4d
Michał <dydmichal@gmail.com>
2023-12-08 17:23:10 +0100

    lab5

---
+++ lab5/my-react-app/src/Hello.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return(<>
+    <div class="main">
+        <h1>Hello World</h1>
+        <img src="https://t4.ftcdn.net/jpg/01/15/20/75/360_F_115207580_US2etunH78I7iMYHOoNVvxQTCIdoPdRj.jpg"></img>
+        <h1>Pan dał 5</h1>
+    </div>
+    </>
+    );
+    }
